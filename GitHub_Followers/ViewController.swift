80f186d67b4d0907f5bc7187f80bdaf366b865ea
Pedro Augusto <ptssar22@gmail.com>
2026-02-02 10:03:21 -0300

    feat: project setup

---
+++ GitHub_Followers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GitHub_Followers
+//
+//  Created by Pedro Augusto on 02/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
+
