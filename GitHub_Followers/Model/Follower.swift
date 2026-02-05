351157bbb960b7d70fe61a1fc68ab2f165c32d67
Pedro Augusto <ptssar22@gmail.com>
2026-02-05 09:33:22 -0300

    feat: Follower model

---
+++ GitHub_Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub_Followers
+//
+//  Created by Pedro Augusto on 05/02/26.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
