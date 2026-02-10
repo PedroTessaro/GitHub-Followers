f49b77b463d5fbe1d934598241fcedd957db6af1
Pedro Augusto <ptssar22@gmail.com>
2026-02-10 12:27:57 -0300

    feat: first userInfo constraints

--- GitHub_Followers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
+++ GitHub_Followers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -9,8 +9,49 @@ import UIKit
 
 class GFUserInfoHeaderViewController: UIViewController {
 
+    let avatarImageView     = GFAvatarImageView(frame: .zero)
+    let usernameLabel       = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel           = GFSecondaryTitleLabel(fontSize: 18)
+    let locationImageView   = UIImageView()
+    let locationLabel       = GFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel            = GFBodyLabel(textAlignment: .left)
+    
+    var user: User!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubviews()
+    }
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func addSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(locationLabel)
+        view.addSubview(bioLabel)
+    }
+    
+
+    private func layoutUI() {
+        let padding: CGFloat            = 20
+        let textImagePadding: CGFloat   = 12
+        locationImageView.translatesAutoresizingMaskIntoConstraints = false
         
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+        ])
     }
 }
