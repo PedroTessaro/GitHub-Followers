df50426f3d88d874b3ee895cb26dfa0573e26ca0
Pedro Augusto <ptssar22@gmail.com>
2026-02-10 12:20:45 -0300

    feat: secondary title label

---
+++ GitHub_Followers/CustomViews/ViewControllers/GFUserInfoHeaderViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GFUserInfoHeaderViewController.swift
+//  GitHub_Followers
+//
+//  Created by Pedro Augusto on 10/02/26.
+//
+
+import UIKit
+
+class GFUserInfoHeaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
