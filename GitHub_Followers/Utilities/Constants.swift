e3c0ebb7d12b9128f6086d7ae1cbf461bf5db2bf
Pedro Augusto <ptssar22@gmail.com>
2026-02-11 09:31:35 -0300

    feat: Constants file

---
+++ GitHub_Followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHub_Followers
+//
+//  Created by Pedro Augusto on 11/02/26.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
